e3907596805b39067d87f6e04f75c41fad3b0941
Ilija <gtrifunovic05@gmail.com>
2023-02-17 21:15:46 +0100

    uredio ostatak

--- controllers/prijave.js
+++ controllers/prijave.js
@@ -1,7 +1,6 @@
 const Prijave = require("../models/prijave");
 const asyncWrapper = require("../errors/asyncWrapper.js");
 const customError = require("../errors/customError");
-const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
@@ -41,82 +40,82 @@ const sendEmail = (to, subject, html) => {
 
 const napraviPrijavu = asyncWrapper(async (req, res, next) => {
   let prijava = req.body;
-  // console.log(prijava)
+  console.log(prijava);
 
   // uslovi ukoliko bude trebalo
 
-  // const auth = new google.auth.GoogleAuth({
-  //   keyFile: "credentials.json",
-  //   scopes: "https://www.googleapis.com/auth/spreadsheets",
-  // });
-  // console.log(auth);
-  // const client = await auth.getClient();
-  // const spreadsheetId = "1Ez2gDlspH4PfjsNdwozzuC3G9U2XKOSPZCFfIFtIjmg";
-  // const googleSheets = google.sheets({ version: "v4", auth: client });
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  console.log(auth);
+  const client = await auth.getClient();
+  const spreadsheetId = "1M8VkcA6iv1mBVjo9VZNGYX212TSySPBJ23AmkwcP_E4";
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  // await googleSheets.spreadsheets.values.append({
-  //   auth,
-  //   spreadsheetId,
-  //   range: "Sheet1!A:F",
-  //   valueInputOption: "USER_ENTERED",
-  //   resource: {
-  //     values: [
-  //       [
-  //         prijava.pitanje1,
-  //         prijava.pitanje2,
-  //         prijava.pitanje3,
-  //         prijava.pitanje4,
-  //         prijava.clanovi[0].imePrezime +
-  //           "\n" +
-  //           prijava.clanovi[0].email +
-  //           "\n" +
-  //           prijava.clanovi[0].brojTelefona +
-  //           "\n" +
-  //           prijava.clanovi[0].status +
-  //           "\n" +
-  //           prijava.clanovi[0].imeSkole +
-  //           "\n" +
-  //           prijava.clanovi[0].linkCV,
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:F",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [
+        [
+          prijava.pitanje1,
+          prijava.pitanje2,
+          prijava.pitanje3,
+          prijava.pitanje4,
+          prijava.clanovi[0].imePrezime +
+            "\n" +
+            prijava.clanovi[0].email +
+            "\n" +
+            prijava.clanovi[0].brojTelefona +
+            "\n" +
+            prijava.clanovi[0].status +
+            "\n" +
+            prijava.clanovi[0].imeSkole +
+            "\n" +
+            prijava.clanovi[0].linkCV,
 
-  //         prijava.clanovi[1].imePrezime +
-  //           "\n" +
-  //           prijava.clanovi[1].email +
-  //           "\n" +
-  //           prijava.clanovi[1].brojTelefona +
-  //           "\n" +
-  //           prijava.clanovi[1].status +
-  //           "\n" +
-  //           prijava.clanovi[1].imeSkole +
-  //           "\n" +
-  //           prijava.clanovi[1].linkCV,
+          prijava.clanovi[1].imePrezime +
+            "\n" +
+            prijava.clanovi[1].email +
+            "\n" +
+            prijava.clanovi[1].brojTelefona +
+            "\n" +
+            prijava.clanovi[1].status +
+            "\n" +
+            prijava.clanovi[1].imeSkole +
+            "\n" +
+            prijava.clanovi[1].linkCV,
 
-  //         prijava.clanovi[2].imePrezime +
-  //           "\n" +
-  //           prijava.clanovi[2].email +
-  //           "\n" +
-  //           prijava.clanovi[2].brojTelefona +
-  //           "\n" +
-  //           prijava.clanovi[2].status +
-  //           "\n" +
-  //           prijava.clanovi[2].imeSkole +
-  //           "\n" +
-  //           prijava.clanovi[2].linkCV,
+          prijava.clanovi[2].imePrezime +
+            "\n" +
+            prijava.clanovi[2].email +
+            "\n" +
+            prijava.clanovi[2].brojTelefona +
+            "\n" +
+            prijava.clanovi[2].status +
+            "\n" +
+            prijava.clanovi[2].imeSkole +
+            "\n" +
+            prijava.clanovi[2].linkCV,
 
-  //         prijava.clanovi[3].imePrezime +
-  //           "\n" +
-  //           prijava.clanovi[3].email +
-  //           "\n" +
-  //           prijava.clanovi[3].brojTelefona +
-  //           "\n" +
-  //           prijava.clanovi[3].status +
-  //           "\n" +
-  //           prijava.clanovi[3].imeSkole +
-  //           "\n" +
-  //           prijava.clanovi[3].linkCV,
-  //       ],
-  //     ],
-  //   },
-  // });
+          prijava.clanovi[3].imePrezime +
+            "\n" +
+            prijava.clanovi[3].email +
+            "\n" +
+            prijava.clanovi[3].brojTelefona +
+            "\n" +
+            prijava.clanovi[3].status +
+            "\n" +
+            prijava.clanovi[3].imeSkole +
+            "\n" +
+            prijava.clanovi[3].linkCV,
+        ],
+      ],
+    },
+  });
 
   let emails = "";
   for (let i = 0; i < prijava.clanovi.length; i++) {
