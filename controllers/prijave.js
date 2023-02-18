dbd0c278360a05b7995bbba0e68a8203c23a3309
Ilija <gtrifunovic05@gmail.com>
2023-02-18 22:34:06 +0100

    vercel.json

--- controllers/prijave.js
+++ controllers/prijave.js
@@ -6,10 +6,6 @@ const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
-  type: "SMTP",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PWD,
